ce888d1ea9c6f2d5b3c8c212586ca87a813766d8
chenzhjie <jaychen@m5stack.com>
2023-02-07 16:03:17 +0800

    CODE FORMATTING

--- src/UNIT_EXT_ENCODER.cpp
+++ src/UNIT_EXT_ENCODER.cpp
@@ -1,7 +1,7 @@
 #include "UNIT_EXT_ENCODER.h"
 
 void UNIT_EXT_ENCODER::writeBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
-                              uint8_t length) {
+                                  uint8_t length) {
     _wire->beginTransmission(addr);
     _wire->write(reg);
     for (int i = 0; i < length; i++) {
@@ -11,7 +11,7 @@ void UNIT_EXT_ENCODER::writeBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
 }
 
 void UNIT_EXT_ENCODER::readBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
-                             uint8_t length) {
+                                 uint8_t length) {
     uint8_t index = 0;
     _wire->beginTransmission(addr);
     _wire->write(reg);
@@ -22,8 +22,8 @@ void UNIT_EXT_ENCODER::readBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
     }
 }
 
-bool UNIT_EXT_ENCODER::begin(TwoWire *wire, uint8_t addr, uint8_t sda, uint8_t scl,
-                         uint32_t speed) {
+bool UNIT_EXT_ENCODER::begin(TwoWire *wire, uint8_t addr, uint8_t sda,
+                             uint8_t scl, uint32_t speed) {
     _wire  = wire;
     _addr  = addr;
     _sda   = sda;
@@ -37,7 +37,7 @@ bool UNIT_EXT_ENCODER::begin(TwoWire *wire, uint8_t addr, uint8_t sda, uint8_t s
         return true;
     } else {
         return false;
-    }     
+    }
 }
 
 uint32_t UNIT_EXT_ENCODER::getEncoderValue(void) {
@@ -45,7 +45,8 @@ uint32_t UNIT_EXT_ENCODER::getEncoderValue(void) {
 
     uint8_t reg = UNIT_EXT_ENCODER_ENCODER_REG;
     readBytes(_addr, reg, data, 4);
-    uint32_t value = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+    uint32_t value =
+        data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
     return value;
 }
 
@@ -54,7 +55,8 @@ uint32_t UNIT_EXT_ENCODER::getZeroPulseValue(void) {
 
     uint8_t reg = UNIT_EXT_ENCODER_ZERO_PULSE_VALUE_REG;
     readBytes(_addr, reg, data, 4);
-    uint32_t value = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+    uint32_t value =
+        data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
     return value;
 }
 
@@ -63,15 +65,16 @@ uint32_t UNIT_EXT_ENCODER::getMeterValue(void) {
 
     uint8_t reg = UNIT_EXT_ENCODER_METER_REG;
     readBytes(_addr, reg, data, 4);
-    uint32_t value = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+    uint32_t value =
+        data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
     return value;
 }
 
-void UNIT_EXT_ENCODER::getMeterString(char* str) {
+void UNIT_EXT_ENCODER::getMeterString(char *str) {
     char read_buf[9] = {0};
 
     uint8_t reg = UNIT_EXT_ENCODER_METER_STRING_REG;
-    readBytes(_addr, reg, (uint8_t*)read_buf, 9);
+    readBytes(_addr, reg, (uint8_t *)read_buf, 9);
     memcpy(str, read_buf, sizeof(read_buf));
 }
 
@@ -79,7 +82,7 @@ void UNIT_EXT_ENCODER::resetEncoder(void) {
     uint8_t data[8] = {0};
 
     uint8_t reg = UNIT_EXT_ENCODER_RESET_REG;
-    data[0] = 1;
+    data[0]     = 1;
     writeBytes(_addr, reg, data, 1);
 }
 
@@ -87,10 +90,10 @@ void UNIT_EXT_ENCODER::setPerimeter(uint32_t perimeter) {
     uint8_t data[8] = {0};
 
     uint8_t reg = UNIT_EXT_ENCODER_PERIMETER_REG;
-    data[0] = (perimeter & 0xff);
-    data[1] = ((perimeter >> 8)& 0xff);
-    data[2] = ((perimeter >> 16)& 0xff);
-    data[3] = ((perimeter >> 24)& 0xff);
+    data[0]     = (perimeter & 0xff);
+    data[1]     = ((perimeter >> 8) & 0xff);
+    data[2]     = ((perimeter >> 16) & 0xff);
+    data[3]     = ((perimeter >> 24) & 0xff);
     writeBytes(_addr, reg, data, 4);
 }
 
@@ -99,7 +102,8 @@ uint32_t UNIT_EXT_ENCODER::getPerimeter(void) {
 
     uint8_t reg = UNIT_EXT_ENCODER_PERIMETER_REG;
     readBytes(_addr, reg, data, 4);
-    uint32_t value = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+    uint32_t value =
+        data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
     return value;
 }
 
@@ -107,10 +111,10 @@ void UNIT_EXT_ENCODER::setPulse(uint32_t pulse) {
     uint8_t data[8] = {0};
 
     uint8_t reg = UNIT_EXT_ENCODER_PULSE_REG;
-    data[0] = (pulse & 0xff);
-    data[1] = ((pulse >> 8)& 0xff);
-    data[2] = ((pulse >> 16)& 0xff);
-    data[3] = ((pulse >> 24)& 0xff);
+    data[0]     = (pulse & 0xff);
+    data[1]     = ((pulse >> 8) & 0xff);
+    data[2]     = ((pulse >> 16) & 0xff);
+    data[3]     = ((pulse >> 24) & 0xff);
     writeBytes(_addr, reg, data, 4);
 }
 
@@ -119,7 +123,8 @@ uint32_t UNIT_EXT_ENCODER::getPulse(void) {
 
     uint8_t reg = UNIT_EXT_ENCODER_PULSE_REG;
     readBytes(_addr, reg, data, 4);
-    uint32_t value = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+    uint32_t value =
+        data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
     return value;
 }
 
@@ -127,7 +132,7 @@ uint8_t UNIT_EXT_ENCODER::setI2CAddress(uint8_t addr) {
     _wire->beginTransmission(_addr);
     _wire->write(I2C_ADDRESS_REG);
     _wire->write(addr);
-    _wire->endTransmission();    
+    _wire->endTransmission();
     _addr = addr;
     return _addr;
 }
@@ -135,11 +140,11 @@ uint8_t UNIT_EXT_ENCODER::setI2CAddress(uint8_t addr) {
 uint8_t UNIT_EXT_ENCODER::getI2CAddress(void) {
     _wire->beginTransmission(_addr);
     _wire->write(I2C_ADDRESS_REG);
-    _wire->endTransmission();  
+    _wire->endTransmission();
 
     uint8_t RegValue;
 
-    _wire->requestFrom(_addr, 1); 
+    _wire->requestFrom(_addr, 1);
     RegValue = Wire.read();
     return RegValue;
 }
@@ -147,11 +152,11 @@ uint8_t UNIT_EXT_ENCODER::getI2CAddress(void) {
 uint8_t UNIT_EXT_ENCODER::getFirmwareVersion(void) {
     _wire->beginTransmission(_addr);
     _wire->write(FIRMWARE_VERSION_REG);
-    _wire->endTransmission();  
+    _wire->endTransmission();
 
     uint8_t RegValue;
 
-    _wire->requestFrom(_addr, 1); 
+    _wire->requestFrom(_addr, 1);
     RegValue = Wire.read();
     return RegValue;
 }
--- src/UNIT_EXT_ENCODER.h
+++ src/UNIT_EXT_ENCODER.h
@@ -4,16 +4,16 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define UNIT_EXT_ENCODER_ADDR                        0x59
-#define UNIT_EXT_ENCODER_ENCODER_REG                 0x00
-#define UNIT_EXT_ENCODER_METER_REG                   0x10
-#define UNIT_EXT_ENCODER_METER_STRING_REG            0x20
-#define UNIT_EXT_ENCODER_RESET_REG                   0x30
-#define UNIT_EXT_ENCODER_PERIMETER_REG               0x40
-#define UNIT_EXT_ENCODER_PULSE_REG                   0x50
-#define UNIT_EXT_ENCODER_ZERO_PULSE_VALUE_REG        0x60
-#define FIRMWARE_VERSION_REG                         0xFE
-#define I2C_ADDRESS_REG                              0xFF
+#define UNIT_EXT_ENCODER_ADDR                 0x59
+#define UNIT_EXT_ENCODER_ENCODER_REG          0x00
+#define UNIT_EXT_ENCODER_METER_REG            0x10
+#define UNIT_EXT_ENCODER_METER_STRING_REG     0x20
+#define UNIT_EXT_ENCODER_RESET_REG            0x30
+#define UNIT_EXT_ENCODER_PERIMETER_REG        0x40
+#define UNIT_EXT_ENCODER_PULSE_REG            0x50
+#define UNIT_EXT_ENCODER_ZERO_PULSE_VALUE_REG 0x60
+#define FIRMWARE_VERSION_REG                  0xFE
+#define I2C_ADDRESS_REG                       0xFF
 
 class UNIT_EXT_ENCODER {
    private:
@@ -31,13 +31,13 @@ class UNIT_EXT_ENCODER {
     uint32_t getEncoderValue(void);
     uint32_t getZeroPulseValue(void);
     uint32_t getMeterValue(void);
-    void getMeterString(char* str); 
+    void getMeterString(char* str);
     void resetEncoder(void);
     void setPerimeter(uint32_t perimeter);
     uint32_t getPerimeter(void);
     void setPulse(uint32_t pulse);
     uint32_t getPulse(void);
-    uint8_t getFirmwareVersion(void); 
+    uint8_t getFirmwareVersion(void);
     uint8_t setI2CAddress(uint8_t addr);
     uint8_t getI2CAddress(void);
 };
