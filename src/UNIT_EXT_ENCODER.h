690c72b35a172ae97d05025343c360cea1392a24
chenzhjie <jaychen@m5stack.com>
2023-05-12 14:59:08 +0800

    update unit_ext_encoder.h

--- src/UNIT_EXT_ENCODER.h
+++ src/UNIT_EXT_ENCODER.h
@@ -12,6 +12,7 @@
 #define UNIT_EXT_ENCODER_PERIMETER_REG        0x40
 #define UNIT_EXT_ENCODER_PULSE_REG            0x50
 #define UNIT_EXT_ENCODER_ZERO_PULSE_VALUE_REG 0x60
+#define UNIT_EXT_ENCODER_ZERO_MODE_REG        0x70
 #define FIRMWARE_VERSION_REG                  0xFE
 #define I2C_ADDRESS_REG                       0xFF
 
@@ -30,12 +31,14 @@ class UNIT_EXT_ENCODER {
                uint8_t sda = 21, uint8_t scl = 22, uint32_t speed = 100000L);
     uint32_t getEncoderValue(void);
     uint32_t getZeroPulseValue(void);
+    void setZeroPulseValue(uint32_t value);
     uint32_t getMeterValue(void);
     void getMeterString(char* str);
     void resetEncoder(void);
     void setPerimeter(uint32_t perimeter);
     uint32_t getPerimeter(void);
     void setPulse(uint32_t pulse);
+    void setZeroMode(uint8_t mode);
     uint32_t getPulse(void);
     uint8_t getFirmwareVersion(void);
     uint8_t setI2CAddress(uint8_t addr);
